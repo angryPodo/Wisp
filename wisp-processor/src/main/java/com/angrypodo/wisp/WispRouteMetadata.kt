640e20f8a009b95f0b010625bd8421132a32406f
Jong <jm991014@gmail.com>
2025-11-10 17:09:40 +0900

    feat: data models for route processing

---
+++ wisp-processor/src/main/java/com/angrypodo/wisp/WispRouteMetadata.kt
@@ -0,0 +1,18 @@
+package com.angrypodo.wisp
+
+/**
+ * KSClassDeclaration을 대체하는 순수 Kotlin 모델입니다.
+ */
+internal data class RouteClassInfo(
+    val qualifiedName: String?,
+    val simpleName: String,
+    val annotations: List<AnnotationInfo>,
+)
+
+/**
+ * KSAnnotation을 대체하는 순수 Kotlin 모델입니다.
+ */
+internal data class AnnotationInfo(
+    val qualifiedName: String?,
+    val shortName: String,
+)
