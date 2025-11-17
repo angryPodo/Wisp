909006dadae3926aa9b33aa22c38fe40107db3db
hanminjae <hanminjae131@gmail.com>
2025-11-17 22:17:44 +0900

    refactor: update RouteInfo to use sealed interface with specific data classes for object and class route types

--- wisp-processor/src/main/java/com/angrypodo/wisp/model/RouteInfo.kt
+++ wisp-processor/src/main/java/com/angrypodo/wisp/model/RouteInfo.kt
@@ -4,14 +4,32 @@ import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.TypeName
 
 /**
- * @Wisp 라우트의 생성에 필요한 정보를 담는 순수 Kotlin 데이터 클래스입니다.
+ * 라우트 정보를 타입에 따라 분리하여 표현하는 Sealed Interface 입니다.
  */
-internal data class RouteInfo(
-    val routeClassName: ClassName,
-    val factoryClassName: ClassName,
-    val parameters: List<ParameterInfo>,
+sealed interface RouteInfo {
+    val routeClassName: ClassName
+    val factoryClassName: ClassName
     val wispPath: String
-)
+}
+
+/**
+ * 파라미터가 없는 object/data object 타입 라우트의 정보입니다.
+ */
+internal data class ObjectRouteInfo(
+    override val routeClassName: ClassName,
+    override val factoryClassName: ClassName,
+    override val wispPath: String
+) : RouteInfo
+
+/**
+ * 생성자 파라미터가 있는 class/data class 타입 라우트의 정보입니다.
+ */
+internal data class ClassRouteInfo(
+    override val routeClassName: ClassName,
+    override val factoryClassName: ClassName,
+    override val wispPath: String,
+    val parameters: List<ParameterInfo>
+) : RouteInfo
 
 /**
  * 라우트 생성자 파라미터 정보를 표현하는 데이터 클래스입니다.
