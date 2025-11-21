c49a4932500f1730ae548e35311d995a470810a2
Jong <jm991014@gmail.com>
2025-11-22 01:27:28 +0900

    add: registry lookup function

--- wisp-processor/src/main/java/com/angrypodo/wisp/generator/WispRegistryGenerator.kt
+++ wisp-processor/src/main/java/com/angrypodo/wisp/generator/WispRegistryGenerator.kt
@@ -5,6 +5,7 @@ import com.angrypodo.wisp.WispClassName.ROUTE_FACTORY
 import com.angrypodo.wisp.model.RouteInfo
 import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.KModifier
 import com.squareup.kotlinpoet.MAP
 import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
@@ -14,6 +15,8 @@ import com.squareup.kotlinpoet.TypeSpec
 
 internal object WispRegistryGenerator {
     private const val REGISTRY_NAME = "WispRegistry"
+    private const val FACTORIES_PROPERTY_NAME = "factories"
+    private const val GET_FACTORY_FUN_NAME = "getRouteFactory"
 
     fun generate(routes: List<RouteInfo>): FileSpec {
         val mapType = MAP.parameterizedBy(STRING, ROUTE_FACTORY)
@@ -28,14 +31,22 @@ internal object WispRegistryGenerator {
 
         initializerBlock.unindent().add(")")
 
-        val factoriesProperty = PropertySpec.builder("factories", mapType)
-            .addModifiers(KModifier.INTERNAL)
+        val factoriesProperty = PropertySpec.builder(FACTORIES_PROPERTY_NAME, mapType)
+            .addModifiers(KModifier.PRIVATE)
             .initializer(initializerBlock.build())
+            .build()
+
+        val getFactoryFun = FunSpec.builder(GET_FACTORY_FUN_NAME)
+            .addModifiers(KModifier.INTERNAL)
+            .addParameter("path", STRING)
+            .returns(ROUTE_FACTORY.copy(nullable = true))
+            .addStatement("return %N[path]", factoriesProperty)
             .build()
 
         val registryObject = TypeSpec.objectBuilder(REGISTRY_NAME)
             .addModifiers(KModifier.INTERNAL)
             .addProperty(factoriesProperty)
+            .addFunction(getFactoryFun)
             .build()
 
         return FileSpec.builder(GENERATED_PACKAGE, REGISTRY_NAME)
