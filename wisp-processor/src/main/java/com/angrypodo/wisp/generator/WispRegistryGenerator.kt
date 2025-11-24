5281474f0d28967884381a92f0270bb4616fc622
hanminjae <hanminjae131@gmail.com>
2025-11-24 23:22:06 +0900

    refactor: remove getRoutePattern function from WispRegistryGenerator to simplify route pattern handling

--- wisp-processor/src/main/java/com/angrypodo/wisp/generator/WispRegistryGenerator.kt
+++ wisp-processor/src/main/java/com/angrypodo/wisp/generator/WispRegistryGenerator.kt
@@ -1,8 +1,6 @@
 package com.angrypodo.wisp.generator
 
 import com.angrypodo.wisp.WispClassName
-import com.angrypodo.wisp.model.ClassRouteInfo
-import com.angrypodo.wisp.model.ObjectRouteInfo
 import com.angrypodo.wisp.model.RouteInfo
 import com.squareup.kotlinpoet.ANY
 import com.squareup.kotlinpoet.CodeBlock
@@ -28,7 +26,6 @@ internal class WispRegistryGenerator {
             .addModifiers(KModifier.PUBLIC)
             .addProperty(factoriesProperty)
             .addFunction(buildCreateRouteFun(factoriesProperty))
-            .addFunction(buildGetRoutePatternFun(routes))
             .build()
 
         return FileSpec.builder(WispClassName.GENERATED_PACKAGE, registryName)
@@ -73,52 +70,5 @@ internal class WispRegistryGenerator {
                     .build()
             )
             .build()
-    }
-
-    private fun buildGetRoutePatternFun(routes: List<RouteInfo>): FunSpec {
-        return FunSpec.builder("getRoutePattern")
-            .addModifiers(KModifier.OVERRIDE)
-            .addParameter("route", ANY)
-            .returns(STRING.copy(nullable = true))
-            .apply {
-                val whenBlock = CodeBlock.builder()
-                    .beginControlFlow("return when (route)")
-                routes.forEach { routeInfo ->
-                    val routePattern = buildRoutePatternString(routeInfo)
-                    whenBlock.addStatement("is %T -> %S", routeInfo.routeClassName, routePattern)
-                }
-                whenBlock.addStatement("else -> null")
-                    .endControlFlow()
-                addCode(whenBlock.build())
-            }
-            .build()
-    }
-
-    private fun buildRoutePatternString(routeInfo: RouteInfo): String {
-        return when (routeInfo) {
-            is ClassRouteInfo -> {
-                val pathParams = routeInfo.parameters
-                    .filter { param ->
-                        !param.isNullable && routeInfo.wispPath.contains(
-                            "{${param.name}}"
-                        )
-                    }
-                    .joinToString("") { "/{${it.name}}" }
-
-                val queryParams = routeInfo.parameters
-                    .filterNot { param ->
-                        !param.isNullable && routeInfo.wispPath.contains(
-                            "{${param.name}}"
-                        )
-                    }
-                    .joinToString("&") { "${it.name}={${it.name}}" }
-
-                val canonical = routeInfo.routeClassName.canonicalName
-                val query = if (queryParams.isNotEmpty()) "?$queryParams" else ""
-
-                return "$canonical$pathParams$query"
-            }
-            is ObjectRouteInfo -> routeInfo.routeClassName.canonicalName
-        }
     }
 }
